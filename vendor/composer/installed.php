6a682772e0ed0b55b86c01d09f745c848320b687
gyarmex <gyarmex@gmail.com>
2024-12-09 06:52:45 +0900

    Update composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kouheidesu/php-heroku-app2',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '129c4b00d3e3b384f55725e6635ce87873f9dd33',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kouheidesu/php-heroku-app2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '129c4b00d3e3b384f55725e6635ce87873f9dd33',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
