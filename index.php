8a7d05d994e0e150fe41e2d80cfdb09898947e9a
gyarmex <gyarmex@gmail.com>
2024-12-21 11:55:41 +0900

    Update index page

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 // PHP部分: フォームのデータを処理
+header("Location: /index.html");
+exit();
+
 $message = '';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$name = htmlspecialchars($_POST['name']); // ユーザー入力を安全に処理
