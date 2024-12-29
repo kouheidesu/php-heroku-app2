3253fa70610092e2c8db1d5afcc7e723c74abef1
gyarmex <gyarmex@gmail.com>
2024-12-29 09:32:04 +0900

    Update index page

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // PHP部分: フォームのデータを処理
-// header("Location: /index.html");
-// exit();
+header("Location: /index.html");
+exit();
 
 // 特別な変数がPOSTであれば変数nameに代入
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
