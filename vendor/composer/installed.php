bca8bf83cc31981665db6caf38239394c7b7bf83
gyarmex <gyarmex@gmail.com>
2025-01-03 09:40:09 +0900

    .gitignore追加

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'kouheidesu/php-heroku-app2',
+        'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '6a682772e0ed0b55b86c01d09f745c848320b687',
+        'reference' => '2c45bce4ba85b5af32d753b43358c413195af411',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => false,
+        'dev' => true,
     ),
     'versions' => array(
-        'kouheidesu/php-heroku-app2' => array(
+        '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '6a682772e0ed0b55b86c01d09f745c848320b687',
+            'reference' => '2c45bce4ba85b5af32d753b43358c413195af411',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
